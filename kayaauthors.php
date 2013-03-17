517c82ee67fbef7b99360710305d4857e0728de5
midnightschildren <amar@codisattva.com>
2013-03-17 01:55:19 -0700

    Fix to Book slider (tighten up space across all break points)
    Also made Content slider slightly smaller 5%

--- kayaauthors.php
+++ kayaauthors.php
@@ -4,11 +4,31 @@ Template Name: Kaya Authors Page
 */
 ?>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
-
+<h2 class="diaspora white center">kaya publishes books of the <span class="green">asian pacific diaspora</span></h2>
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 <h2><?php the_title(); ?></h2>
 <?php the_content(); ?>
 
 <?php endwhile; ?>
+<div id="quote" class="grid-whole">
+<div class="grid-1 s-hidden m-hidden">&nbsp;</div>
+<div class="grid-1 s-hidden">&nbsp;</div>
+<div class="padded-inner grid-12 s-grid-16 m-grid-14 center">
 
+<?php
+$quote_query = new WP_Query( array(
+        'post_type' => 'quotes',
+        'posts_per_page' => 1,
+        'orderby' => 'rand'
+        
+        
+    ) )
+    ?>
+<?php if ($quote_query->have_posts()) : while ($quote_query->have_posts()) : $quote_query->the_post(); ?>
+	<h2><?php the_title(); ?></h2>
+<?php endwhile; endif; ?>	
+</div>
+<div class="grid-1 s-hidden">&nbsp;</div>
+<div class="grid-1 s-hidden m-hidden">&nbsp;</div>
+</div>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
