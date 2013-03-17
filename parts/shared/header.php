3c96bb08da460709d6162c1679a07d2e3f65ca8c
midnightschildren <amar@codisattva.com>
2013-03-17 02:48:05 -0700

    Reinserting the logo for mobile layout

--- parts/shared/header.php
+++ parts/shared/header.php
@@ -13,10 +13,10 @@
 <div class="grid-whole navbar">
 	<div class="grid-1 s-hidden m-hidden">&nbsp;</div>
 	<div class="grid-3 s-hidden m-grid-4 left padded-topn"><h6><a href="/">#<?php bloginfo( 'name' ); ?></a> est. 1994</h6></div>
-	<div class="grid-8 s-grid-13 m-grid-8 right padded-topm"><?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?></div>
-	<div class="grid-3 s-grid-3 m-grid-4 right">
-		<div class="grid-9 m-grid-half s-hidden right padded-topo"><img class="alignnone size-tiger" alt="tiger-logo" src="http://kaya.codisattva.com/wp-content/uploads/2013/02/tiger-logo.png" width="80" height="65" /></div>
-		<div class="grid-7 m-grid-half s-grid-16 right padded-tops">
+	<div class="grid-8 s-grid-12 m-grid-8 right padded-topm"><?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?></div>
+	<div class="grid-3 s-grid-4 m-grid-4 right">
+		<div class="grid-9 m-grid-half s-grid-8 right padded-topo"><img class="alignnone size-tiger" alt="tiger-logo" src="http://kaya.codisattva.com/wp-content/uploads/2013/02/tiger-logo.png" width="80" height="65" /></div>
+		<div class="grid-7 m-grid-half s-grid-8 right padded-tops">
 		<a href="#" rel="toggle[searchgroup]" class="searchlink" title="search">SEARCH</a> <img class="s-hidden" alt="search-glass" style="margin-top:-4px;" src="http://kaya.codisattva.com/wp-content/uploads/2013/02/eyeglass.png" width="18" height="18" /></div>
 	</div>
 	<div class="grid-1 s-hidden m-hidden">&nbsp;</div>
