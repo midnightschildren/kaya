30bdb83e0c0f8baef82b80e72c6dcac6414e1d1e
midnightschildren <amar@codisattva.com>
2013-08-11 03:31:14 -0700

    Author Sidebar

--- single-authors.php
+++ single-authors.php
@@ -11,8 +11,38 @@
 ?>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+<?php $single_amar = clone $wp_query; ?>
 
+
+
+<div id="single-book" class="grid-whole background-white">
+<div class="grid-1 m-hidden s-hidden">&nbsp;</div>
+<div class="grid-14 s-grid-16 m-grid-16">
+
+<?php
+
+    $author_term_query = new WP_Query( array(
+        'post_type' => 'authors',
+        'posts_per_page' => -1,
+        'orderby' => 'rand'
+        
+    ) )
+    ?>
+
+<ul> 
+    <?php
+    if ( $author_term_query->have_posts() ) : while ( $author_term_query->have_posts() ) : $author_term_query->the_post(); ?>
+    
+<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+
+	<?php endwhile; endif; ?>
+</ul>
+
+<?php wp_reset_postdata();?>
+
+<?php $author_term_query = null; $wp_query = clone $single_amar; ?><?php rewind_posts(); ?>
+
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 <article>
 
 	<h2><?php the_title(); ?></h2>
@@ -28,6 +58,11 @@
 	
 
 </article>
+
+</div>
+<div class="grid-1 m-hidden s-hidden">&nbsp;</div>
+</div>
+
 <?php endwhile; ?>
 
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
